f5e58435a2227179b98b29e921151679944ea8c5
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-21 13:15:32 -0600

    Delete roadtrips when deleting users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,6 @@ end
 UserRoadtrip.create([
   { user_id: 1, roadtrip_id: 1},
   { user_id: 1, roadtrip_id: 2},
-  { user_id: 2, roadtrip_id: 2},
   { user_id: 2, roadtrip_id: 3},
   { user_id: 2, roadtrip_id: 5},
   { user_id: 3, roadtrip_id: 4}
