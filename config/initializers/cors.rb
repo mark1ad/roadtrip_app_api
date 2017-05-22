25f6c19ef6f688b0399fce76d3aad4339450fb82
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-22 17:18:15 -0600

    trying to fix cors on heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,18 +14,18 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'localhost:3001'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins '*'
 
     resource '*',
     headers: :any,
     methods: [:get, :options, :head]
   end
-
-  # allow do
-  #   origins 'https://open-highway.herokuapp.com', 'localhost:3001'
-  #
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  # end
 end
